eb132049d093318fa30982f0bb2d70ebc20ef7a4
Lennart Marien <lmarien@posteo.net>
2022-06-13 22:42:50 +0200

    Add function to load data for the split model cf approach.

--- modeling/train.py
+++ modeling/train.py
@@ -7,6 +7,7 @@ import pickle
 import warnings
 import mlflow
 from mlflow.sklearn import save_model  # , log_model
+import numpy as np
 
 from feature_engineering import (
     fill_missing_values,
@@ -22,97 +23,51 @@ from config import TRACKING_URI, EXPERIMENT_NAME
 warnings.filterwarnings("ignore")
 logger = getLogger(__name__)
 
+def group_interactions(transactions):
+    int = transactions.groupby(['customer_id', 'article_id']).t_dat.count().reset_index()
+    return int.rename(columns={'t_dat':'interactions'})
 
-def __get_data():
-    logger.info("Getting the data")
-    # coffee data
-    url = "https://github.com/jldbc/coffee-quality-database/raw/master/data/robusta_data_cleaned.csv"
-    coffee_features = pd.read_csv(url)
+def load_and_filter_data(query_str):
+    """Loads the data and derives the interactions as well as utility matrix dimensions needed to instantiate and train the models.
+    Args:
+        query_str (string) : query string for seasonal (or other) filtering
+        
+    Returns:
+        tuple(6) : Returns interaction series, number of users and items for both the seasonal and the full model.
+    """
+    # import transactions, customer and article data
+    transactions = pd.read_csv('data/transactions_train.csv', parse_dates=[0], dtype={'article_id':'string'})
+    articles = pd.read_csv('data/articles.csv', dtype={'article_id':'string'})
+    customers = pd.read_csv('data/customers.csv')
 
-    # coffee score
+    # exclude validation week from training data
+    transactions = transactions.query('t_dat < "2020-09-02"')
+    # filter data to include only purchases in (for previous years) and right before the validation week to reflect seasonal buying    
+    trans_seas = transactions.query(query_str).copy()
+    # count customers and articles that are predictable with seasonal and full model respectively
+    # the full model is trained on the full transaction table (excluding the validation week)
+    n_seasonal = trans_seas.customer_id.nunique()
+    m_seasonal = trans_seas.article_id.nunique()
+    n_full = transactions.customer_id.nunique()
+    m_full = transactions.article_id.nunique()
+    n_all = customers.customer_id.nunique()
+    m_all = articles.article_id.nunique()
+    n_cold = n_all - n_full
 
-    url = "https://raw.githubusercontent.com/jldbc/coffee-quality-database/master/data/robusta_ratings_raw.csv"
-    coffee_quality = pd.read_csv(url)
-
-    # cleaning data and preparing
-    Y = coffee_quality["quality_score"]
-    X = coffee_features.select_dtypes(["number"])
-
-    # splittin into train and test
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, Y, test_size=0.30, random_state=42
-    )
-    ## in order to exemplify how the predict will work.. we will save the y_train
-    logger.info("Saving test data in the data folder .. wo feat eng")
-    X_test.to_csv("data/X_test.csv", index=False)
-    y_test.to_csv("data/y_test.csv", index=False)
-
-    logger.info("Feature engineering on train")
-    X_train = transform_altitude(X_train)
-    X_train = drop_column(X_train, col_name="Unnamed: 0")
-    X_train = drop_column(X_train, col_name="Quakers")
-    X_train = fill_missing_values(X_train)
-
-    # feature eng on test data
-    logger.info("Feature engineering on test")
-    X_test = transform_altitude(X_test)
-    X_test = drop_column(X_test, col_name="Unnamed: 0")
-    X_test = drop_column(X_test, col_name="Quakers")
-    X_test = fill_missing_values(X_test)
-
-    return X_train, X_test, y_train, y_test
-
-
-def __compute_and_log_metrics(
-    y_true: pd.Series, y_pred: pd.Series, prefix: str = "train"
-):
-    mse = mean_squared_error(y_true, y_pred)
-    r2 = r2_score(y_true, y_pred)
-
-    logger.info(
-        "Linear Regression performance on "
-        + str(prefix)
-        + " set: MSE = {:.1f}, R2 = {:.1%},".format(mse, r2)
-    )
-    mlflow.log_metric(prefix + "-" + "MSE", mse)
-    mlflow.log_metric(prefix + "-" + "R2", r2)
-
-    return mse, r2
+    logger.info(f'Number of customers that will be predicted based on the seasonal model: {n_seasonal} ({np.round(n_seasonal/n_all*100.0, decimals=2)} %).')
+    logger.info(f'Number of predictable articles for these customers: {m_seasonal} ({np.round(m_seasonal/m_all*100.0, decimals=2)} %).')
+    logger.info(f'Number of customers that will be predicted based on the full model: {n_full} ({np.round(n_full/n_all*100.0, decimals=2)} %).')
+    logger.info(f'Number of predictable articles for these customers: {m_full} ({np.round(m_full/m_all*100.0, decimals=2)} %).')
+    logger.info(f'Number of cold customers: {n_cold} ({np.round(n_cold/n_all*100.0, decimals=2)} %).')
+    # group interactions (purchases) per customer and article
+    int_seas = group_interactions(trans_seas)
+    int_full = group_interactions(transactions)
+    
+    return int_seas, n_seasonal, m_seasonal, int_full, n_full, m_full
 
 
 def run_training():
-    logger.info(f"Getting the data")
-    X_train, X_test, y_train, y_test = __get_data()
-
-    logger.info("Training simple model and tracking with MLFlow")
-    mlflow.set_tracking_uri(TRACKING_URI)
-    mlflow.set_experiment(EXPERIMENT_NAME)
-    # model
-    logger.info("Training a simple linear regression")
-    with mlflow.start_run():
-        reg = LinearRegression().fit(X_train, y_train)
-        # taking some parameters out of the feature eng.. in your case you can use the params from CV
-        params = {
-            "altitude_low_meters_mean": altitude_low_meters_mean,
-            "altitude_high_meters_mean": altitude_high_meters_mean,
-            "altitude_mean_log_mean": altitude_mean_log_mean,
-            "fit_intercept": True,
-        }
-        mlflow.log_params(params)
-        mlflow.set_tag("worst_model", "True")
-        y_train_pred = reg.predict(X_train)
-
-        __compute_and_log_metrics(y_train, y_train_pred)
-
-        y_test_pred = reg.predict(X_test)
-        __compute_and_log_metrics(y_test, y_test_pred, "test")
-
-        logger.info("this is obviously fishy")
-        # saving the model
-        # logger.info("Saving model in the model folder")
-        # path = "models/linear"
-        # save_model(sk_model=reg, path=path)
-        # logging the model to mlflow will not work without a AWS Connection setup.. too complex for now
+    pass
 
 
 if __name__ == "__main__":
@@ -123,4 +78,5 @@ if __name__ == "__main__":
     logging.getLogger("pyhive").setLevel(logging.CRITICAL)  # avoid excessive logs
     logger.setLevel(logging.INFO)
 
-    run_training()
+    query_str = '(t_dat >= "2018-08-22" and t_dat < "2018-09-23") or (t_dat >= "2019-08-22" and t_dat < "2019-09-23") or (t_dat >= "2020-08-01" and t_dat < "2020-09-02")'
+    load_and_filter_data(query_str)
