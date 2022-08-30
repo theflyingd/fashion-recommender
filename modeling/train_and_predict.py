ca7ee581c52e8568773b371adaa8a7031c1343c3
Lennart Marien <lmarien@posteo.net>
2022-08-30 17:19:57 +0200

    Implement json config file to reduce hardcoding and declutter the code. Some changes for more DRY and clarity as well.

--- modeling/train_and_predict.py
+++ modeling/train_and_predict.py
@@ -1,20 +1,12 @@
 from logging import getLogger
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.linear_model import LinearRegression
-import pickle
 import warnings
-import mlflow
-from mlflow.sklearn import save_model  # , log_model
 import numpy as np
 from scipy.sparse import coo_matrix
-import joblib
-import os
+import os, sys
 from implicit.als import AlternatingLeastSquares
 from tqdm import tqdm
-
-from config import TRACKING_URI, EXPERIMENT_NAME
+import json
 
 warnings.filterwarnings("ignore")
 logger = getLogger(__name__)
@@ -31,18 +23,18 @@ def group_interactions(transactions):
     int = transactions.groupby(['customer_id', 'article_id']).t_dat.count().reset_index()
     return int.rename(columns={'t_dat':'interactions'})
 
-def load_and_filter_data(query_str):
+def load_and_filter_data(query_str, config):
     """Loads the data and derives the interactions as well as utility matrix dimensions needed to instantiate and train the models.
     Args:
         query_str (string) : query string for seasonal (or other) filtering
-
+        config : dict containing file paths 
     Returns:
-        tuple(6) : Returns interaction frame, number of users and items for both the seasonal and the full model.
+        tuple(4) : Returns interaction frame, model dimensions as well as transactions and customers
     """
     # import transactions, customer and article data
-    transactions = pd.read_csv('data/transactions_train.csv', parse_dates=[0], dtype={'article_id':'string'})
-    articles = pd.read_csv('data/articles.csv', dtype={'article_id':'string'})
-    customers = pd.read_csv('data/customers.csv')
+    transactions = pd.read_csv(config['transactions'], parse_dates=[0], dtype={'article_id':'string'})
+    articles = pd.read_csv(config['articles'], dtype={'article_id':'string'})
+    customers = pd.read_csv(config['customers'])
 
     # filter data to include only purchases close to validation period ("seasonal" model)    
     transactions = transactions.query(query_str).copy()
@@ -89,9 +81,9 @@ def get_utility_matrix(ints, n_users, n_items):
 
     return Y_csr, sparsity, user_ids, user_id_map, item_id_map_rev
 
-def get_als_model(Y, n_factors=1280, reg=0.01, it=30):
+def get_als_model(Y, n_factors=1280, reg=0.01, it=30, use_gpu=False):
     # instantiate and train model
-    model = AlternatingLeastSquares(factors=n_factors, regularization=reg, num_threads=0, iterations=it, use_gpu=False)
+    model = AlternatingLeastSquares(factors=n_factors, regularization=reg, num_threads=0, iterations=it, use_gpu=use_gpu)
     model.fit(2 * Y)    
     return model
 
@@ -101,40 +93,48 @@ def idx_to_ids(item_ids, user_ids, map_reverse):
     return tmp.apply(lambda s: ' '.join(s.apply(lambda id: map_reverse[id])), axis=1)
 
 
-def run_training(n_factors=1280, reg=0.01, it=30):
+def run_training(config_file):
     logger.info('Loading and filtering data...')
     # define training periods based on the given test weeks and make query strings from them
-    test_weeks = [{'start_date':'{}-08-26', 'end_date':'{}-09-01'}, {'start_date':'{}-09-02', 'end_date':'{}-09-08'},
-    {'start_date':'{}-09-09', 'end_date':'{}-09-15'}, {'start_date':'{}-09-16', 'end_date':'{}-09-22'}, {'start_date':'{}-09-23', 'end_date':'{}-09-29'}] 
-    query_strings = ['', '', '', '', '']
-    query_strings_full = ['', '', '', '', '']
-    for i, week in enumerate(test_weeks):
-        # window centered around and including the test week for the past years
-        for year in [2018, 2019]:
+    with open(config_file) as json_file:
+        config = json.load(json_file)
+
+    n_factors = config['n_factors']
+    reg = config['reg']
+    it = config['it']
+
+    n_weeks = len(config['test_weeks'])
+    train_years = range(config['test_year']-2, config['test_year']+1)
+    query_strings = [''] * n_weeks
+    query_strings_full = [''] * n_weeks
+
+    for i, week in enumerate(config["test_weeks"]):
+        # window centered around and including the test week for the past 2 years 
+        for year in train_years:
             start_date = pd.to_datetime(week['start_date'].format(year), yearfirst=True)
             end_date = pd.to_datetime(week['end_date'].format(year), yearfirst=True)
-            sd = start_date - pd.Timedelta(12, 'D')
-            ed = end_date + pd.Timedelta(12, 'D')
-            query_strings[i] += f'(t_dat >= "{sd}" and t_dat <= "{ed}") or '
-        # for the current year the test week needs to be excluded and no future data from after the test week can be used
-        start_date = pd.to_datetime(week['start_date'].format(2020), yearfirst=True)
-        end_date = pd.to_datetime(week['end_date'].format(2020), yearfirst=True)
-        sd = start_date - pd.Timedelta(30, 'D')                
-        query_strings[i] += f'(t_dat >= "{sd}" and t_dat < "{start_date}")'
-        query_strings_full[i] = f't_dat < "{start_date}"'
+            if year < config['test_year']:
+                sd = start_date - pd.Timedelta(12, 'D')
+                ed = end_date + pd.Timedelta(12, 'D')
+                query_strings[i] += f'(t_dat >= "{sd}" and t_dat <= "{ed}") or '
+            else:
+                # for the current year the test week needs to be excluded and no future data from after the test week can be used
+                sd = start_date - pd.Timedelta(30, 'D')                
+                query_strings[i] += f'(t_dat >= "{sd}" and t_dat < "{start_date}")'
+                query_strings_full[i] = f't_dat < "{start_date}"'
 
-    # prepare, train and save models        
+    # prepare, train and predict
     logger.info(f'Instantiating and training models using {n_factors} factors with regularization strength {reg} and up {it} iterations...')
 
     # repeat for each test week
     for i, qs in enumerate(query_strings):
         logger.info(f'Working on seasonal model for test week {i+1}...')
-        int_seas, dim_seas, _, _ = load_and_filter_data(qs)
+        int_seas, dim_seas, _, _ = load_and_filter_data(qs, config)
 
         # create utility matrix
         Ys, sparse_seas, user_ids, user_id_map, item_id_map_rev  = get_utility_matrix(int_seas, dim_seas['n'], dim_seas['m'])    
         logger.info(f'Utility matrix of seasonal model for test week {i+1} has dimensions ({dim_seas["n"]}, {dim_seas["m"]}) and a sparsity factor of {np.round(sparse_seas, 2)}.')    
-        rs = get_als_model(Ys, n_factors=n_factors, reg=reg, it=it)        
+        rs = get_als_model(Ys, n_factors=n_factors, reg=reg, it=it, use_gpu=config['use_gpu'])        
 
         # predict full batch of users
         logger.info(f'Predicting first batch of users for test week {i+1}...')
@@ -143,10 +143,10 @@ def run_training(n_factors=1280, reg=0.01, it=30):
 
         # get interaction data for full model (everything before test week) and train model
         logger.info(f'Working on full model for test week {i+1}...')
-        int_full, dim_full, transactions, customers = load_and_filter_data(query_strings_full[i])        
+        int_full, dim_full, transactions, customers = load_and_filter_data(query_strings_full[i], config)        
         Ys_full, sparse_full, _, user_id_map_full, item_id_map_rev_full = get_utility_matrix(int_full, dim_full['n'], dim_full['m'])    
         logger.info(f'Utility matrix of full model has dimensions ({dim_full["n"]}, {dim_full["m"]}) and a sparsity factor of {np.round(sparse_full, 2)}.')
-        rs_full = get_als_model(Ys_full, n_factors=n_factors, reg=reg, it=it)    
+        rs_full = get_als_model(Ys_full, n_factors=n_factors, reg=reg, it=it, use_gpu=config['use_gpu'])    
 
         # find all customers that have not been predicted by the seasonal model due to lack of data, but can be predicted with full model
         logger.info(f'Predicting second batch of users for test week {i+1}...')
@@ -164,10 +164,9 @@ def run_training(n_factors=1280, reg=0.01, it=30):
         submission = customers.join(submission, on='customer_id', how='left').set_index('customer_id')
 
         # now fill empty predictions (cold starts) with baseline and write to file
-        baseline_prediction = '0706016001 0706016002 0372860001 0610776002 0759871002 0464297007 0372860002 0610776001 0399223001 0706016003 0720125001 0156231001'
+        baseline_prediction = config['global_baseline']
         submission.fillna(baseline_prediction, inplace=True)
-        filename = f'prediction_test_week_{i+1}_ALS_{n_factors}_factors_r_{reg}_maxit_{it}2.csv'
-        submission.loc[:, 'prediction'].to_csv(os.path.join('data/', filename))
+        submission.loc[:, 'prediction'].to_csv(config["file_out"])
         logger.info(f'Done with test week {i+1}...')
     logger.info('Done...')
     
@@ -182,4 +181,16 @@ if __name__ == "__main__":
     logging.getLogger("pyhive").setLevel(logging.CRITICAL)  # avoid excessive logs
     logger.setLevel(logging.INFO)
 
-    run_training()
+    # check command line arguments
+    if len(sys.argv) == 1:
+        logger.warning('No config file provided. Trying default...')
+        config_file = 'modeling/config.json'
+    else:
+        config_file = sys.argv[1]
+
+    config_file = str(config_file)
+    if not os.path.exists(config_file):
+        logger.warning('Config file does not seem to exist. Exiting...')
+        raise(FileNotFoundError())
+
+    run_training(config_file)
